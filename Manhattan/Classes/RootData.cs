88ca6887283569b5fb6abb352317d5e6a8e02f5b
abood alomary <abboodoa@gmail.com>
2023-07-11 02:54:29 +0300

    Lab09 done and all finished

---
+++ Manhattan/Classes/RootData.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manhattan.Classes
+{
+    public class RootData
+    {
+        public string type { get; set; }
+        public List<FeatureClass> features { get; set; }
+
+        
+    }
+}
